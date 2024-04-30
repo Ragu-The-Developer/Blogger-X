254d4ee69c3c5e2da5175e237af2f103dc8a62e2
Ragu-The-Developer <ragugithub@gmail.com>
2024-04-30 14:55:26 +0530

    first commit

--- src/components/joinsection.jsx
+++ src/components/joinsection.jsx
@@ -1,31 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react'
 
-
-function App() {
-  const [email, setEmail] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // You can add your newsletter signup logic here
-    console.log(`Submitting email: ${email}`);
-    setEmail('');
-  };
-
+export default function joinsection() {
   return (
     <div className="newsletter">
       <h1 className='news-title'>Subscribe to Newsletter</h1>
-      <form onSubmit={handleSubmit} className='form-panel' method="post" action="process_form.php">
+      <form className='form-panel' method="post" >
         <input
           type="email" name='email'
           placeholder="Enter your email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
           required
         />
         <button type="submit" value="Submit">Subscribe</button>
       </form>
-    </div>
-  );
-}
 
-export default App;
+      
+
+      
+
+      
+    </div>
+  )
+}
